e9ada386acc6ec9f3260c5d146b3bc4721a024c6
LucioMSP <luciomsp@geeks.ms>
2018-05-18 17:50:35 -0500

    Codigo ejemplo DeviceID

---
+++ Demo_DeviceID/Demo_DeviceID/Demo_DeviceID/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+using Plugin.DeviceInfo;
+
+
+
+namespace Demo_DeviceID
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var deviceId = CrossDeviceInfo.Current.Id;
+            var deviceIdResult = new Label();
+
+            deviceIdResult.Text = deviceId;
+
+            titleDeviceId.Text = "My device Id: " + deviceIdResult.Text;
+        }
+    }
+}
