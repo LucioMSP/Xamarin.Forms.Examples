b1da0939255653c72f80093bd65887b5dddbe6c0
LucioMSP <luciomsp@geeks.ms>
2018-05-18 16:36:50 -0500

    Codigo ejemplo SplashScreen

---
+++ SplashScreenDemo/SplashScreenDemo/SplashScreenDemo.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.OS;
+using Android.App;
+using Android.Content.PM;
+
+namespace SplashScreenDemo.Droid
+{
+    [Activity(Label = "SplashScreenDemo", Icon = "@drawable/icon", Theme = "@style/MainTheme", 
+        MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ SplashScreenDemo/SplashScreenDemo/SplashScreenDemo.Android/SplashActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.OS;
+using Android.App;
+
+namespace SplashScreenDemo.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
