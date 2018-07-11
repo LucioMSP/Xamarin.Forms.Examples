bb2d379e2fddeda7f5eb521a471d5cd6d6512ce9
LucioMSP <luciomsp@geeks.ms>
2018-07-11 12:09:05 -0500

    Codigo ejemplo Zoom Image

---
+++ ZoomImageDemo/ZoomImageDemo/ZoomImageDemo/ZoomExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZoomImageDemo
+{
+    public static class ZoomExtension
+    {
+        public static double Clamp(this double self, double min, double max)
+        {
+            return Math.Min(max, Math.Max(self, min));
+        }
+    }
+}
---
+++ ZoomImageDemo/ZoomImageDemo/ZoomImageDemo/ZoomGestureContainer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ZoomImageDemo
+{
+    public class ZoomGestureContainer : ContentView
+    {
+        double currentScale = 1;
+        double startScale = 1;
+        double xOffset = 0;
+        double yOffset = 0;
+
+        public ZoomGestureContainer()
+        {
+            var pinchGesture = new PinchGestureRecognizer();
+            pinchGesture.PinchUpdated += OnPinchUpdated;
+            GestureRecognizers.Add(pinchGesture);
+        }
+
+        void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Status == GestureStatus.Started)
+            {
+                startScale = Content.Scale;
+                Content.AnchorX = 0;
+                Content.AnchorY = 0;
+            }
+            if (e.Status == GestureStatus.Running)
+            {
+                currentScale += (e.Scale - 1) * startScale;
+                currentScale = Math.Max(1, currentScale);
+                
+                double renderedX = Content.X + xOffset;
+                double deltaX = renderedX / Width;
+                double deltaWidth = Width / (Content.Width * startScale);
+                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+
+                double renderedY = Content.Y + yOffset;
+                double deltaY = renderedY / Height;
+                double deltaHeight = Height / (Content.Height * startScale);
+                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+
+                double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
+                double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
+
+                Content.TranslationX = targetX.Clamp(-Content.Width * (currentScale - 1), 0);
+                Content.TranslationY = targetY.Clamp(-Content.Height * (currentScale - 1), 0);
+
+                Content.Scale = currentScale;
+            }
+            if (e.Status == GestureStatus.Completed)
+            {
+                xOffset = Content.TranslationX;
+                yOffset = Content.TranslationY;
+            }
+        }
+    }
+}
