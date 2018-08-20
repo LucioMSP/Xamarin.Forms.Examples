a8d4e1158da48a6f1a1784b2b5b617ed16cbcee1
LucioMSP <luciomsp@geeks.ms>
2018-08-20 12:41:50 -0500

    Codigo ejemplo Consumiendo RestFul Service

---
+++ RestFul_Demo/RestFul_Demo/RestFul_Demo.Android/obj/Debug/android/src/md50ebc6ced60439ac0047b1bc0a69c1203/MainActivity.java
@@ -0,0 +1,47 @@
+package md50ebc6ced60439ac0047b1bc0a69c1203;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("RestFul_Demo.Droid.MainActivity, RestFul_Demo.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("RestFul_Demo.Droid.MainActivity, RestFul_Demo.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ RestFul_Demo/RestFul_Demo/RestFul_Demo/PostRestPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace RestFul_Demo
+{
+    public class Post
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PostRestPage : ContentPage
+	{
+        private const string url = "https://jsonplaceholder.typicode.com/posts";
+        private HttpClient _Client = new HttpClient();
+        private ObservableCollection<Post> _post;
+
+        public PostRestPage ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override async void OnAppearing()
+        {
+            var content = await _Client.GetStringAsync(url);
+            var post = JsonConvert.DeserializeObject<List<Post>>(content);
+            _post = new ObservableCollection<Post>(post);
+            Post_List.ItemsSource = _post;
+            base.OnAppearing();
+        }
+    }
+}
