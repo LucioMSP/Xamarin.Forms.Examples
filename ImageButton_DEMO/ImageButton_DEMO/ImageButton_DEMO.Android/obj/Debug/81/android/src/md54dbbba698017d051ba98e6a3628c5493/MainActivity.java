2973c0211a35dcbf485a27ba361af444203ea27c
LucioMSP <luciomsp@geeks.ms>
2018-12-27 12:45:32 -0600

    Codigo de ejemplo ImageButton Control

---
+++ ImageButton_DEMO/ImageButton_DEMO/ImageButton_DEMO.Android/obj/Debug/81/android/src/md54dbbba698017d051ba98e6a3628c5493/MainActivity.java
@@ -0,0 +1,47 @@
+package md54dbbba698017d051ba98e6a3628c5493;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("ImageButton_DEMO.Droid.MainActivity, ImageButton_DEMO.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("ImageButton_DEMO.Droid.MainActivity, ImageButton_DEMO.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ ImageButton_DEMO/ImageButton_DEMO/ImageButton_DEMO/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ImageButton_DEMO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("DEMO", "Has presionado un control ImageButton", "OK");
+        }
+    }
+}
