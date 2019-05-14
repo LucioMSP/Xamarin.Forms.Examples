17548c9348c20b965c3323f96dfc15b23fe2046f
LucioMSP <luciomsp@geeks.ms>
2019-05-13 21:53:41 -0500

    Codigo ejemplo WhatsApp en XF

---
+++ WAXF/WAXF.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "e4309533-a416-485b-b4b9-b6d8290140ff",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
---
+++ WAXF/WAXF/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using System.ComponentModel;
+using Xamarin.Forms.OpenWhatsApp;
+
+namespace WAXF
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenWhatsApp(object sender, EventArgs e)
+        {
+            try
+            {
+                Chat.Open("+525543484042", "Envia este texto... VGGL");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+}
