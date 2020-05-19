9607a640360b1cff3aa80407665c0261136f9964
Vicente Guzman <luciomsp@geeks.ms>
2020-05-18 22:06:37 -0500

    Sensitive Data Example

---
+++ SensitiveData/SensitiveData/SensitiveData.Android/obj/Debug/android/src/crc649a7cdd175fb5a024/MainActivity.java
@@ -0,0 +1,74 @@
+package crc649a7cdd175fb5a024;
+
+
+public class MainActivity
+	extends crc643f46942d9dd1fff9.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"";
+		mono.android.Runtime.register ("SensitiveData.Droid.MainActivity, SensitiveData.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("SensitiveData.Droid.MainActivity, SensitiveData.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ SensitiveData/SensitiveData/SensitiveData.iOS/AppDelegate.cs
@@ -0,0 +1,45 @@
+﻿using UIKit;
+using Foundation;
+using System.Linq;
+
+namespace SensitiveData.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+
+        public override void OnResignActivation(UIApplication application)
+        {
+            var blurEffect = UIBlurEffect.FromStyle(UIBlurEffectStyle.ExtraDark);
+            var blurEffectView = new UIVisualEffectView(blurEffect)
+            {
+                Frame = application.KeyWindow.Subviews.First().Bounds,
+                AutoresizingMask = UIViewAutoresizing.FlexibleDimensions,
+                Tag = 12
+            };
+            application.KeyWindow.Subviews.Last().AddSubview(blurEffectView);
+            base.OnResignActivation(application);
+        }
+
+        public override void OnActivated(UIApplication uiApplication)
+        {
+            var sub = uiApplication.KeyWindow?.Subviews.Last();
+            if (sub == null)
+                return;
+            foreach (var vv in sub.Subviews)
+            {
+                if (vv.Tag == 12)
+                    vv.RemoveFromSuperview();
+            }
+            base.OnActivated(uiApplication);
+        }
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
