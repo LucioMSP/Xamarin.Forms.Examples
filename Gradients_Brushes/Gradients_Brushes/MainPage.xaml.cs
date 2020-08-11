ef0085b5cb2dfdad2c932bf4184522e289fe9af0
Vicente Guzman <luciomsp@geeks.ms>
2020-08-10 21:02:58 -0500

    Xamarin.Forms 4.8 - Gradients and Brushes

---
+++ Gradients_Brushes/Gradients_Brushes/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace Gradients_Brushes
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Gradients_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Views.GradientsView());
+        }
+    }
+}
---
+++ Gradients_Brushes/Gradients_Brushes/Views/GradientsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Gradients_Brushes.Views
+{
+    public partial class GradientsView : ContentPage
+    {
+        public GradientsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
